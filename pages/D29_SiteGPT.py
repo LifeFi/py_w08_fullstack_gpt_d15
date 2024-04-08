b151f570e4369363b038a51d4ab582e64de392cb
topcircle <topcircle@gmail.com>
2024-04-08 19:17:57 +0900

    add YYMMDD to datetime log

--- pages/D29_SiteGPT.py
+++ pages/D29_SiteGPT.py
@@ -27,7 +27,7 @@ st.session_state["run_count"] += 1
 
 start_time = datetime.now()
 print(
-    f"\n\033[43mSTART Exec[{st.session_state['run_count']}]: {start_time.strftime('%H:%M:%S.%f')} =========================================\033[0m"
+    f"\n\033[43mSTART Exec[{st.session_state['run_count']}]: {start_time.strftime('%Y-%m-%d %H:%M:%S.%f')} ===============================\033[0m"
 )
 
 st.set_page_config(
@@ -387,5 +387,5 @@ end_time = datetime.now()
 elapsed_time = end_time - start_time
 elapsed_seconds = elapsed_time.total_seconds()
 print(
-    f"\n\033[43mEND Exec[{st.session_state['run_count']}]: {elapsed_seconds}s / {end_time.strftime('%H:%M:%S.%f')} =========================================\033[0m"
+    f"\n\033[43mEND Exec[{st.session_state['run_count']}]: {elapsed_seconds}s / {end_time.strftime('%Y-%m-%d %H:%M:%S.%f')} ===============================\033[0m"
 )
